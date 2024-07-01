ffdc1bf4a77dea1f701f7f2c367e269d8b8660c0
FrankiNarvaez <frannki03@gmail.com>
2024-06-30 22:06:18 -0500

    Resolviendo merge

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </BrowserRouter>,
 )
 
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
-
